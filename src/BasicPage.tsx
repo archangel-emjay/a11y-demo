301697db77a27f25d0207f750de3dfc7c5f26068
Michael Johnston <michaeljohnston@gmx.co.uk>
2024-06-18 13:27:15 +0100

    Update titles

--- src/BasicPage.tsx
+++ src/BasicPage.tsx
@@ -2,29 +2,32 @@ import { useState } from "react";
 
 const BasicPage = () => {
     const [tcAgree, setTcAgree] = useState<boolean>(false);
-    return <form>
-        <label>
-            <input type="checkbox" />
-            Email
-        </label>
-        <br />
-        <label>
-            <input type="checkbox" />
-            SMS
-        </label>
-        <br />
-        <label>
-            <input type="checkbox" />
-            Semophore
-        </label>
-        <br />
-        <label>
-            <input type="checkbox" value={tcAgree.toString()} onChange={() => setTcAgree(!tcAgree)} />
-            I have read and accepted the <a href="/#">Terms and Conditions</a>
-        </label>
-        <br/>
-        <input type="submit" disabled={!tcAgree} value='Submit'/>
-    </form>
+    return (<>
+        <h1>Change your communication preferences</h1>
+        <form>
+            <label>
+                <input type="checkbox" />
+                Email
+            </label>
+            <br />
+            <label>
+                <input type="checkbox" />
+                SMS
+            </label>
+            <br />
+            <label>
+                <input type="checkbox" />
+                Semaphore
+            </label>
+            <br />
+            <label>
+                <input type="checkbox" value={tcAgree.toString()} onChange={() => setTcAgree(!tcAgree)} />
+                I have read and accepted the <a href="/#">Terms and Conditions</a>
+            </label>
+            <br/>
+            <input type="submit" disabled={!tcAgree} value='Submit'/>
+        </form>
+    </>);
 };
 
 export default BasicPage;
